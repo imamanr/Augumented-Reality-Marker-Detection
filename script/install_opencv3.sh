435521e59445e689798af5a178e124080213c54d
imama <imama@urus.com>
2017-08-14 13:18:01 -0700

    circleCI updated

--- script/install_opencv3.sh
+++ script/install_opencv3.sh
@@ -2,4 +2,4 @@ sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pk
 
 
 
-mkdir opencv && cd opencv && wget https://github.com/Itseez/opencv/archive/3.0.0-alpha.zip -O opencv-3.0.0-alpha.zip && unzip opencv-3.0.0-alpha.zip && cd opencv-3.0.0-alpha && mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local .. && make -j6 && sudo make install
+mkdir opencv && cd opencv && wget https://github.com/Itseez/opencv/archive/3.0.0-alpha.zip -O opencv-3.0.0-alpha.zip && unzip opencv-3.0.0-alpha.zip && cd opencv-3.0.0-alpha && mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_IPP=OFF .. && make -j6 && sudo make install
