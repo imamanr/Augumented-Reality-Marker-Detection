36df404e2f3522ec69d29cc3fc0b89981c3a69a2
imama <imama@urus.com>
2017-08-14 14:28:48 -0700

    circleCI updated

--- script/install_opencv3.sh
+++ script/install_opencv3.sh
@@ -1,5 +1,3 @@
 sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
 
-
-
-git clone https://github.com/Itseez/opencv.git && cd opencv && git checkout 3.2.0 && mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_IPP=OFF .. && make -j6 && sudo make install
+git clone https://github.com/Itseez/opencv.git && cd opencv && git checkout 3.2.0 && mkdir build && cd build && cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_IPP=OFF .. && make -j6 && sudo make install && sudo ldconfig
